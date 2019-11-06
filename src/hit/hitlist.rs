8a3c05020b97e54d2f12e1de810f2ecadfb4956f
Guillaume Depardon <guillaume.depardon@gmail.com>
2019-11-06 04:24:13 +0100

    Simplified HitList

--- src/hit/hitlist.rs
+++ src/hit/hitlist.rs
@@ -5,39 +5,27 @@ use crate::prelude::ParallelHit;
 
 pub struct HitList<T>(Vec<T>);
 
-impl HitList<Box<dyn ParallelHit>> {
-    pub fn new_dyn(list: Vec<Box<dyn ParallelHit>>) -> Self {
-        Self(list)
-    }
-}
-
 impl<T: ParallelHit> Hit for HitList<T> {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
-        let mut record = None;
         let mut closest_so_far = t_max;
 
-        for hit in &self.0 {
-            if let Some(rec) = hit.hit(ray, t_min, closest_so_far) {
+        self.0.iter()
+            .filter_map(|hit| {
+                let rec = hit.hit(ray, t_min, closest_so_far)?;
                 closest_so_far = rec.t;
-                record = Some(rec);
-            }
-        }
-
-        record
+                Some(rec)
+            })
+            .last()
     }
 
     fn bounding_box(&self, t0: f32, t1: f32) -> Option<AABB> {
-        if self.0.is_empty() {
-            return None
-        }
+        let mut hits = self.0.iter();
 
-        let mut bbox = self.0[0].bounding_box(t0, t1)?;
+        let bbox = hits.next()?.bounding_box(t0, t1)?;
 
-        for obj in self.0.iter().skip(1) {
-            let temp_box = obj.bounding_box(t0, t1)?;
-            bbox = AABB::surrounding_box(bbox, temp_box);
-        }
-
-        Some(bbox)
+        hits.try_fold(bbox, |bbox_so_far, obj| {
+            let bbox = obj.bounding_box(t0, t1)?;
+            Some(AABB::surrounding_box(bbox_so_far, bbox))
+        })
     }
 }
