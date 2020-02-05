82b8d399333cd39b6fd36648d75aa950cefa22bc
Guillaume Depardon <guillaume.depardon@gmail.com>
2020-02-05 02:12:57 +0100

    Updated demo scene

--- trt-dsl/scenes/demo.py
+++ trt-dsl/scenes/demo.py
@@ -1,23 +1,25 @@
 import trt
 
-red = (1., 0., 0.)
-green = (0., 1., 0.)
-blue = (0., 0., 1.)
+red = (1, 0, 0)
+green = (0, 1, 0)
+blue = (0, 0, 1)
 
-def world():
+def scene():
     spheres = [
         trt.Sphere(
-            center=(-50. + i * 50., 0., 0.),
-            radius=20.,
+            center=(-50 + i * 50, 0, 0),
+            radius=20,
             color=color,
         )
         for i, color in enumerate((red, green, blue))
     ]
 
     ground = trt.Sphere(
-        center=(0., -5050., 0.),
-        radius=5000.,
-        color=(1., 1., 1.),
+        center=(0, -5050, 0),
+        radius=5000,
+        color=(1, 1, 1),
     )
 
-    return spheres + [ground]
+    return trt.Scene(**{
+        "world": spheres + [ground]
+    })
