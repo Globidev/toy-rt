cf9fa74053d0b08bd29ab9919acb9ca8c83728d9
Guillaume Depardon <guillaume.depardon@gmail.com>
2020-02-10 00:07:48 +0100

    Made canvas bounds visible while being computed

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,61 +5,10 @@ import { render } from "react-dom";
 import AceEditor from "react-ace-builds";
 import "react-ace-builds/webpack-resolver-min";
 
-// function onChange(newValue: any) {
-//   console.log("change", newValue);
-// }
-
 export function main() {
   trt.setup_panic_hook();
 
-  let source = window.localStorage.getItem("last-source") || `
-  from trt import Sphere, Rect, Scene, Camera
-
-red = (0.9, 0.3, 0.3)
-green = (0.3, 0.9, 0.3)
-blue = (0.3, 0.3, 0.9)
-white = (0.7, 0.7, 0.7)
-
-def scene():
-    spheres = [
-        Sphere(
-            center=(150+ i * 150, 150 + i * 100, 150 + i * 100),
-            radius=80,
-            color=color,
-        )
-        for i, color in enumerate((red, green, blue))
-    ]
-
-    cornell_box = [
-        # side walls
-        Rect(y=(0, 555), z=(-1000, 555), x=555, color=red).flip_normals(),
-        Rect(y=(0, 555), z=(-1000, 555), x=0, color=blue),
-
-        # ceiling
-        Rect(x=(0, 555), z=(-1000, 555), y=555, color=white).flip_normals(),
-        # floor
-        Rect(x=(0, 555), z=(-1000, 555), y=0, color=green),
-
-        # front
-        Rect(x=(0, 555), y=(0, 555), z=555, color=white).flip_normals(),
-        # back
-        Rect(x=(0, 555), y=(0, 555), z=-1000, color=white),
-    ]
-
-    return Scene(**{
-        "world": cornell_box + spheres,
-        "width": 800,
-        "height": 800,
-        "samples_per_px": 10,
-        "rays_per_sample": 30,
-        "camera": Camera(
-            look_at=(300, 300, 0),
-            look_from=(300, 300, -800),
-        )
-    })
-
-
-  `;
+  let source = window.localStorage.getItem("last-source") || "";
   render(
     <AceEditor
       mode="python"
@@ -125,6 +74,8 @@ def scene():
     canvas.setAttribute("width", scene.width().toString())
     canvas.setAttribute("height", scene.height().toString())
 
+    canvas.classList.add('canvas-building')
+
     for (let worker of workers) {
       worker.postMessage({ type: 'scene', code: source })
     }
@@ -148,6 +99,7 @@ def scene():
         setTimeout(dispatchWork, 0);
       } else {
         console.log(Date.now() - start)
+        canvas.classList.remove('canvas-building')
       }
     }
 
