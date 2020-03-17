155aad87a54de3333214c440b2db9daabc4c31b2
Guillaume Depardon <guillaume.depardon@gmail.com>
2020-03-17 15:24:29 +0100

    DRY'd up a utility method

--- trt-dsl/src/future.rs
+++ trt-dsl/src/future.rs
@@ -20,6 +20,6 @@ impl<T: 'static + Clone> PyFuture<T> {
     }
 
     pub fn map<U: 'static + Clone>(self, f: impl FnOnce(T) -> U + 'static) -> PyFuture<U> {
-        PyFuture(self.0.map(f).boxed_local().shared())
+        PyFuture::new(self.0.map(f))
     }
 }
