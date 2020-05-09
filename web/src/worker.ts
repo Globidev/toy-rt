11546ad4fbf5fddd78ca887aa9355ddb80a01d04
Guillaume Depardon <guillaume.depardon@gmail.com>
2020-05-09 14:46:36 +0200

    Completely reworked the webworker code using comlink

--- web/src/worker.ts
+++ web/src/worker.ts
@@ -1,50 +1,41 @@
-type Scene = import('trt').Scene;
+import { expose as comlinkExpose } from "comlink";
 
-import("trt")
-  .then(wasm => {
-    console.log("WORKER LOADED")
+import { PythonVM, Scene } from "trt";
+
+type SceneSize = { width: number; height: number };
+
+export class WasmWorker {
+  vm: PythonVM | null = null;
+  scene: Scene | null = null;
+
+  async init() {
+    let wasm = await import("trt");
 
     wasm.setup_panic_hook();
+    this.vm = wasm.PythonVM.new();
+  }
 
-    let scene: Scene | null = null
-    let backlog: any | null = null
+  async compileScene(code: string): Promise<SceneSize | null> {
+    if (this.vm) {
+      const result = this.vm.eval(code);
 
-    const compute = (scene: Scene, e: any) => {
-      const row = e.data.row
-      const colors = scene.row_color(row);
-      self.postMessage({ type: 'row', colors, row, height: scene.height() })
-    }
+      let scene: Scene | null = await result.build_scene();
 
-    const vm = wasm.PythonVM.new();
-
-    self.postMessage({ type: "ready" })
-
-    self.onmessage = async (e: any) => {
-      switch (e.data.type) {
-        case "scene":
-          scene = null
-          const scenePromise = vm.eval(e.data.code);
-          if (scenePromise === undefined)
-            return;
-          scene = await scenePromise.build_scene() as Scene
-          if (backlog !== null) {
-            compute(scene, backlog)
-          }
-          break
-
-        case "compute":
-          if (scene == null) {
-            backlog = e
-            return
-          }
-          compute(scene, e)
-          break
-
-        default: break
+      if (scene) {
+        this.scene = scene;
+        return { width: scene.width(), height: scene.height() };
       }
     }
-  })
-  .catch(err => {
-    console.error("Error importing wasm:", err);
-    self.postMessage({ type: 'error', why: `Error importing wasm: ${err}` })
-  });
+    return null;
+  }
+
+  compute(row: number) {
+    return this.scene?.row_color(row);
+  }
+
+  eval(source: string) {
+    return this.vm?.eval(source).data();
+  }
+}
+
+comlinkExpose(new WasmWorker());
