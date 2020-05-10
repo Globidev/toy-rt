1c22717fb583ff4e9263d484fb407727f73639d5
Guillaume Depardon <guillaume.depardon@gmail.com>
2020-05-10 15:09:50 +0200

    Remove wasm loading from the main UI thread

--- web/src/bootstrap.ts
+++ web/src/bootstrap.ts
@@ -1,15 +1,3 @@
-declare global {
-  var trt: typeof import('trt');
-  var wasm_memory: any;
-}
-
 import { main } from "./index";
 
-export async function loadWasm() {
-  const trt = await import('trt');
-
-  window.trt = trt;
-}
-
-loadWasm()
-  .then(main);
+main();
