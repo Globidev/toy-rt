eae7ba7e76684bf4fc583dee0ad7aab6803b0dbc
Guillaume Depardon <guillaume.depardon@gmail.com>
2020-05-10 15:21:21 +0200

    Hooked worker events to the control panel

--- web/src/components/control-panel.tsx
+++ web/src/components/control-panel.tsx
@@ -10,13 +10,20 @@ import "jquery.terminal";
 import "jquery.terminal/css/jquery.terminal.min.css";
 import "jquery.terminal/js/prism.js";
 
+import { WasmExecutor } from "../wasm-executor";
+import { WorkerState, EvalResult } from "../worker";
+
+import * as Comlink from "comlink";
+
 interface IControlPanelProps {
-  onEval: (source: string) => Promise<string>;
-  onRunScript: () => Promise<void>;
+  onEval: (source: string) => Promise<EvalResult>;
+  onRunScript: () => Promise<EvalResult>;
+  wasmExecutor: WasmExecutor;
 }
 
 interface IControlPanelState {
   running: boolean;
+  workerStates: WorkerState[];
 }
 
 export class ControlPanel extends React.Component<
@@ -24,28 +31,74 @@ export class ControlPanel extends React.Component<
   IControlPanelState
 > {
   termRef = React.createRef<HTMLDivElement>();
-  state: IControlPanelState = { running: false };
+  term: JQueryTerminal | null = null;
+  state: IControlPanelState = {
+    running: false,
+    workerStates: [],
+  };
+
+  constructor(props: IControlPanelProps) {
+    super(props);
+
+    this.wireWorkers();
+  }
+
+  wireWorkers() {
+    this.props.wasmExecutor.workers.forEach((worker, id) => {
+      let statePromise: Promise<WorkerState> = worker.state; // Typescript issue: Promise<A> | Promise<B> can't be coerced to a Promise<A | B>
+      statePromise.then((state) => this.changeWorkerState(id, state));
+
+      worker.setOnStateChange(
+        Comlink.proxy((state) => this.changeWorkerState(id, state))
+      );
+    });
+  }
+
+  changeWorkerState(id: number, state: WorkerState) {
+    let workerStates = this.state.workerStates.slice(0);
+    workerStates[id] = state;
+    let newState = { ...this.state, workerStates };
+    this.setState(newState);
+  }
 
   componentDidMount() {
     const termElement = this.termRef.current;
     const self = this;
 
     if (termElement != null) {
-      $(termElement).terminal(
+      this.term = $(termElement).terminal(
         async (source, term) => {
-          try {
-            let res = await self.props.onEval(source);
-            term.echo(res);
-          } catch (error) {
-            term.error(error);
+          if (this.state.running) {
+            return;
+          }
+          term.freeze(true);
+          this.setState({ running: true });
+          let result = await self.props.onEval(source);
+          this.setState({ running: false });
+          term.freeze(false);
+          switch (result.kind) {
+            case "success":
+              if (result.returnValue != "None") term.echo(result.returnValue);
+              break;
+
+            case "error":
+              term.error(result.error);
+              break;
           }
         },
         {
           greetings: "Toy RT v1.0",
           prompt: "> ",
+          completion: () => self.props.wasmExecutor.getLocalNames(),
+          clear: false,
         }
       );
+
       $.terminal.syntax("python");
+
+      this.props.wasmExecutor.onEvalError = (error) => {
+        this.term?.error(error);
+      };
     }
   }
 
@@ -56,18 +109,64 @@ export class ControlPanel extends React.Component<
           <div style={{ flex: 0.1, margin: "5px" }}>
             <button
               onClick={async () => {
+                this.term?.freeze(true);
                 this.setState({ running: true });
                 await this.props.onRunScript();
                 this.setState({ running: false });
+                this.term?.freeze(false);
               }}
               disabled={this.state.running}
             >
               Run script
             </button>
+            <div className="workers-container">
+              {this.state.workerStates.map((state, id) => {
+                return (
+                  <div key={id} className="worker-state-container">
+                    <span>{`[W${id}] - `}</span>
+                    {StateElement(state)}
+                  </div>
+                );
+              })}
+            </div>
           </div>
           <div ref={this.termRef} style={{ height: "100%", flex: 1 }}></div>
         </div>
       </div>
     );
+  }
+}
+
+function StateElement(state: WorkerState): JSX.Element {
+  switch (state.kind) {
+    case "created":
+      return <span className="status-initial">Created</span>;
+    case "loading":
+      return (
+        <React.Fragment>
+          <span className="status-working">Loading</span>
+          <span> …</span>
+        </React.Fragment>
+      );
+    case "loaded":
+      return <span className="status-done">Loaded</span>;
+    case "idle":
+      return <span className="status-done">Idle</span>;
+    case "compute":
+      return (
+        <React.Fragment>
+          <span className="status-working">Rendering</span>
+          <span> row </span>
+          <span className="status-data">{state.row}</span>
+          <span>…</span>
+        </React.Fragment>
+      );
+    case "eval":
+      return (
+        <React.Fragment>
+          <span className="status-working">Evaluating</span>
+          <span> script …</span>
+        </React.Fragment>
+      );
   }
 }
